77d682054bee237e5820bb4c49806df2f82ce3d5
Brian Mulawka <mulawkabrian@gmail.com>
2025-01-08 10:52:37 +0100

    ajout suppression card

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ window.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById('searchInput');
   const sortByPriorityBtn = document.getElementById('sortByPriorityBtn');
 
+  function DelCard(card) {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.className = "delete-btn";
+    deleteBtn.addEventListener("click", () => {
+      card.remove();
+    });
+    card.appendChild(deleteBtn);
+  }
+  document.querySelectorAll(".card").forEach((card) => {
+    if (!card.querySelector(".delete-btn")) {
+        DelCard(card);
+    }
+  });
+  
   // Éventuellement, on écoute les événements
   addCardBtn.addEventListener('click', () => {
     // ...
